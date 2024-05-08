fd532b956393b2b792dee5307edca82e7df9bed4
gabosin <1234567890aaaaaaabbbbbbb@gmail.com>
2024-05-08 11:59:55 +0200

    register.php

---
+++ register.php
@@ -0,0 +1,23 @@
+<?php
+include 'conexion.php';
+
+$usuario = $_POST['usuario'];
+$pass = $_POST['pass'];
+$hash = password_hash($pass, PASSWORD_DEFAULT);
+$cuser = "SELECT usuario FROM usuarios WHERE usuario = '$usuario'";
+$rcuser = $conn->query($cuser);
+
+if (isset($_POST['Registrarse'])) {
+
+    if ($rcuser->num_rows > 0) {
+        echo "El usuario ya existe";
+    } else {
+        $sql = "INSERT INTO `usuarios` (`usuario`, `pass`) VALUES ('$usuario', '$hash')";
+
+        if ($conn->query($sql) === TRUE) {
+            echo "Registro exitoso";
+        } else {
+            echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+        }
+    }
+}
